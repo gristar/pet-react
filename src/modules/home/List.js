be6cc3e29f4b3fedf0cbb4d9f83600c40c424efa
gristar <sheng.fighting@qq.com>
2017-10-16 18:14:02 +0800

    edit card and cardlist

--- src/modules/home/List.js
+++ src/modules/home/List.js
@@ -4,13 +4,53 @@ import Item from './Item';
 class List extends React.Component {
     render () {
         return (
-            <div>
-                <Item/>
-                <Item/>
-                <Item/>
-                <Item/>
-                <Item/>
-                <Item/>
+            <div style={{paddingBottom: 56}}>
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                    userName="飞行的夏天"
+                />
+                <Item
+                    title="看点点笑得多迷人"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <Item
+                    title="下面由灰灰和毛毛用黄瓜表演吹口琴"
+                    imgs={[{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=1a06f46abfe314a6fe63563fab2a83ce&imgtype=0&src=http%3A%2F%2Fwww.qqtn.com%2Fup%2F2014-5%2F14002206943661130.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149625045&di=2b6cd013b038b7d9d8387849efb52b2b&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-06-23%2F183019605.jpg'},{src:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508149642211&di=ec91447d59b4c31013b0888cc953214f&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1225775914%2C2507975769%26fm%3D214%26gp%3D0.jpg'}]}
+                />
+                <div style={{textAlign: 'center', fontSize: '.8em', color: 'gray'}}>没有更多了</div>
             </div>
         )
     }
